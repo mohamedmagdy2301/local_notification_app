3ca866e3df4f100ef875fe69daec001c2624f647
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-26 22:20:04 +0300

    Done Local Notification Service To Show Notification ( Basic - Repeating - Scheduled)

--- lib/notification_helper/local_notification_service.dart
+++ lib/notification_helper/local_notification_service.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class LocalNotificationService {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
-  static Function(NotificationResponse) onTap = (details) {};
-
+  static onTap(NotificationResponse response) {}
+  //! Initialize Local Notification
   static Future initialize() async {
     InitializationSettings initializationSettings =
         const InitializationSettings(
@@ -16,5 +18,77 @@ class LocalNotificationService {
       onDidReceiveBackgroundNotificationResponse: onTap,
       onDidReceiveNotificationResponse: onTap,
     );
+  }
+
+// ! Show Basic Notification
+  static Future showBasicNotification() async {
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+      'channel_id 0',
+      'basic channel',
+      importance: Importance.max,
+      priority: Priority.high,
+    ));
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Basic Notification',
+      'Body',
+      notificationDetails,
+      payload: 'data',
+    );
+  }
+
+// ! Show Repeating Notification every minute
+  static Future showRepeatingNotification() async {
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+      'channel_id 1',
+      'basic channel',
+      importance: Importance.max,
+      priority: Priority.high,
+    ));
+    await flutterLocalNotificationsPlugin.periodicallyShow(
+      1,
+      'Repeating Notification',
+      'Notification Body',
+      RepeatInterval.everyMinute,
+      notificationDetails,
+      payload: 'data',
+    );
+  }
+
+// ! Show Scheduled Notification 10 seconds
+  static Future showScheduledNotification() async {
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+      'channel_id 2',
+      'basic channel',
+      importance: Importance.max,
+      priority: Priority.high,
+    ));
+    tz.initializeTimeZones();
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      2,
+      'Scheduled Notification',
+      'Body',
+      //? To Get Current Time by local timezone
+      tz.TZDateTime.now(tz.local).add(
+        const Duration(seconds: 10),
+      ),
+      notificationDetails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      payload: 'data',
+    );
+  }
+
+// ! Cancel All Notification
+  static Future cancelAllNotifications() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+// ! Cancel Notification By Id
+  static Future cancelNotificationById(int id) async {
+    await flutterLocalNotificationsPlugin.cancel(id);
   }
 }
