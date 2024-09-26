7b3f200ed5c98549ca2d50145d2556d5a4b8ec5b
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-26 18:39:17 +0300

    create Notification App

---
+++ lib/notification_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationApp extends StatelessWidget {
+  const NotificationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:local_notification_app/main.dart';
+import 'package:local_notification_app/notification_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NotificationApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
