f40eebe2fdbb9eb4fae5edfed2ef5422acdda78c
Mohamed magdy <mohammedmego15@gmail.com>
2024-09-26 22:21:37 +0300

    Done UI widget  and integrate with local notification service

--- lib/notification_app.dart
+++ lib/notification_app.dart
@@ -1,15 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:local_notification_app/notification_helper/local_notification_service.dart';
+import 'package:local_notification_app/widget/button_push_or_cancel.dart';
 
 class NotificationApp extends StatelessWidget {
   const NotificationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(title: const Text("Local Notification App")),
         body: Center(
-          child: Text('Hello, World!'),
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            width: 400,
+            height: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //! Push or Cancel Basic Notification
+                ButtonPushOrCancl(
+                  text: 'Basic',
+                  onPressedToPush: () =>
+                      LocalNotificationService.showBasicNotification(),
+                  onPressedToCancel: () =>
+                      LocalNotificationService.cancelNotificationById(0),
+                ),
+                //! Push or Cancel Repeating Notification
+                ButtonPushOrCancl(
+                  text: 'Repeating',
+                  onPressedToPush: () =>
+                      LocalNotificationService.showRepeatingNotification(),
+                  onPressedToCancel: () =>
+                      LocalNotificationService.cancelNotificationById(1),
+                ),
+                //! Push or Cancel Scheduled Notification
+                ButtonPushOrCancl(
+                  text: 'Scheduled',
+                  onPressedToPush: () =>
+                      LocalNotificationService.showScheduledNotification(),
+                  onPressedToCancel: () =>
+                      LocalNotificationService.cancelNotificationById(2),
+                ),
+                //! Cancel All Notification
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                    ),
+                    onPressed: () =>
+                        LocalNotificationService.cancelAllNotifications(),
+                    child: const Text(
+                      "Cancel All Notification",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/widget/button_push_or_cancel.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ButtonPushOrCancl extends StatelessWidget {
+  const ButtonPushOrCancl({
+    super.key,
+    this.onPressedToPush,
+    required this.text,
+    this.onPressedToCancel,
+  });
+  final Function()? onPressedToPush, onPressedToCancel;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 250,
+          child: ElevatedButton(
+            onPressed: onPressedToPush,
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 50, 132, 9),
+            ),
+            iconAlignment: IconAlignment.start,
+            child: Text(
+              "Show $text Notification",
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(width: 20),
+        IconButton(
+          onPressed: onPressedToCancel,
+          icon: const Icon(
+            Icons.cancel,
+            color: Color.fromARGB(255, 243, 123, 115),
+            size: 40,
+          ),
+        ),
+      ],
+    );
+  }
+}
